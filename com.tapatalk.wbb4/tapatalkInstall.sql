815d035197770903a7a08604feeeba0a1560b93c
Alexander Ebert <ebert@woltlab.com>
2014-03-17 15:28:57 +0100

    Fixed field length
    wcf1_user.userID is defined as INT(10) and a timestamp is only 10 digits long

--- com.tapatalk.wbb4/tapatalkInstall.sql
+++ com.tapatalk.wbb4/tapatalkInstall.sql
@@ -1,13 +1,13 @@
 CREATE TABLE wbb1_tapatalk_push_user (
-	user_id INT(11) NOT NULL PRIMARY KEY,
-	create_time INT(11) NOT NULL DEFAULT '0',
-	update_time INT(11) NOT NULL DEFAULT '0',
+	user_id INT(10) NOT NULL PRIMARY KEY,
+	create_time INT(10) NOT NULL DEFAULT '0',
+	update_time INT(10) NOT NULL DEFAULT '0',
 );
 
 CREATE TABLE wbb1_tapatalk_status (
 	status_info TEXT NOT NULL,
-	create_time INT(11) NOT NULL DEFAULT '0',
-	update_time INT(11) NOT NULL DEFAULT '0'
+	create_time INT(10) NOT NULL DEFAULT '0',
+	update_time INT(10) NOT NULL DEFAULT '0'
 );
 
 ALTER TABLE wbb1_tapatalk_push_user ADD FOREIGN KEY (userID) REFERENCES wcf1_user (userID) ON DELETE CASCADE;
