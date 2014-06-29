74f8b25546857ab47197db2ec1dd80502bf80d93
Sascha Greuel <SoftCreatR@users.noreply.github.com>
2014-06-29 18:37:45 +0200

    Fixed reply push

--- com.tapatalk.wbb4/files/mobiquo/push/TapatalkPush.php
+++ com.tapatalk.wbb4/files/mobiquo/push/TapatalkPush.php
@@ -507,7 +507,7 @@ Class TapatalkPush extends TapatalkBasePush {
                 return false;
             }
         }
-        $query ="SELECT userID FROM wcf".WCF_N."_user_object_watch WHERE objectTypeID = (SELECT objectTypeID FROM wbb".WCF_N."_object_type WHERE objectType = 'com.woltlab.wbb.thread') and objectID = ?";
+        $query ="SELECT userID FROM wcf".WCF_N."_user_object_watch WHERE objectTypeID = (SELECT objectTypeID FROM wcf1_object_type WHERE className = 'wbb\\system\\user\\object\\watch\\ThreadUserObjectWatch') and objectID = ?";
 		$statement = $this->oDb->prepareStatement($query);
 	    $statement->execute(array($topicId));
 	    $userIds = array();
